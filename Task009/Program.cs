e62b31a24044264331b3e8767d59e9c139c5f0d6
Andrey <a89615076005@yandex.ru>
2022-09-24 21:17:41 +0300

    Add task 9

---
+++ Task009/Program.cs
@@ -0,0 +1,19 @@
+﻿//Программа, которая выводит случайное число из отрезка [10,99] и показывает наибольшую цифру числа
+
+Console.Clear();
+
+int number = new Random().Next(10,100);
+
+Console.WriteLine(number);
+
+int firstDigit = number % 10;
+int lastDigit = number / 10;
+
+if (firstDigit > lastDigit)
+{
+    Console.WriteLine(firstDigit);
+}
+else 
+{
+    Console.WriteLine(lastDigit);
+}
