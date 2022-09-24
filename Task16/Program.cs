01e1e2ef0d70e480ed38856ff9846490f5e05d9b
Andrey <a89615076005@yandex.ru>
2022-09-24 22:22:35 +0300

    add changes task 16

---
+++ Task16/Program.cs
@@ -0,0 +1,21 @@
+﻿// Программа, которая принимает на вход два числа и проверяет, является ли одно квадратом другого
+Console.Clear();
+
+Console.Write("Введите первое число: ");
+int number1 = int.Parse(Console.ReadLine());
+
+Console.Write("Введите второе число: ");
+int number2 = int.Parse(Console.ReadLine());
+
+if (number1 / number2 == number2)
+{
+    Console.WriteLine("Число один является квадратом числа два");
+}
+else if (number2 / number1 == number1)
+{
+    Console.WriteLine("Число два является квадратом числа один");
+}
+    else
+{
+    Console.WriteLine("Числа не является квадратами друг друга");
+}
