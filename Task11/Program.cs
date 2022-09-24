dd294cbce358e2d158e4a386626ecd715a1f0166
Andrey <a89615076005@yandex.ru>
2022-09-24 21:25:32 +0300

    add task 11

---
+++ Task11/Program.cs
@@ -0,0 +1,14 @@
+﻿//Программа, которая выводит случайное трехзначное число и удаляет вторую цифру этого числа
+
+Console.Clear();
+
+int number = new Random().Next(99,1000);
+
+Console.WriteLine(number);
+
+int firstDigit = number / 100;
+int lastDigit = number % 10;
+
+//Console.WriteLine($"{firstDigit}{lastDigit}");
+Console.Write(firstDigit);
+Console.Write(lastDigit);
